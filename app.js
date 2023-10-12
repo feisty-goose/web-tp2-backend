90135c38567330e9bd867f4cdcad806c85208d62
LarrieGans <112956859+LarrieGans@users.noreply.github.com>
2023-10-12 15:00:24 -0400

    progres serveur et bd

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const app = express();
+var mysql = require('mysql');
 
-app.use((req, res) => {
+var con = mysql.createConnection({
+    host: "mysql-20063c0d-etudiant-5326.aivencloud.com",
+    user: "user-formatif",
+    password: "AVNS_L3yP0xCFyxAjhiqZ2hw",
+    database: "defaultdb",
+    port: 15397
+});
+
+con.connect(function (err) {
+    if (err) { console.log(err); throw err };
+    console.log("connected");
+});
+
+/*app.use((req, res) => {
     console.log("Message reçu");
     res.json({message: "Success"})
+})*/
+
+app.get('/getAllEvents', (req, res) => {
+    const sqlRes = [];
+    con.query("SELECT * FROM calendar_event", function (err, result, fields) {
+        if (err) {
+            throw err;
+        } else {
+            sqlRes = result;
+            console.log(result);
+        }
+    });
+
+    res.json({ reponseDB: sqlRes })
 })
 
 module.exports = app;
