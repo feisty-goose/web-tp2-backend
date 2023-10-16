30569cb2ae86f8d13e907cf044906933389c1aa8
Jean Sébsatien Marier <2135175@cegeplimoilou.ca>
2023-10-16 13:42:48 -0400

    Ajout endpoint POST signUp

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 var mysql = require("mysql");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 var con = mysql.createConnection({
   host: "mysql-20063c0d-etudiant-5326.aivencloud.com",
   user: "user-formatif",
@@ -9,6 +12,11 @@ var con = mysql.createConnection({
   database: "defaultdb",
   port: 15397,
 });
+
+// TODO: Implement hashing and salting algorithm
+function hashAndSalt(rawTextPassword) {
+  return rawTextPassword;
+}
 
 con.connect(function (err) {
   if (err) {
@@ -36,6 +44,32 @@ app.get("/getAllUsers", (req, res) => {
       res.json({ response: result });
     }
   });
+});
+
+app.post("/signUp", (req, res) => {
+  console.log(req.body); // ?
+  let name = req.body.name;
+  let password = req.body.rawPassword;
+
+  if (name === undefined) return;
+  if (password === undefined) return;
+
+  password = hashAndSalt(password);
+
+  con.query(
+    "INSERT INTO users (name, password) VALUES ( ?, ? );",
+    [name, password],
+    function (err, result, fields) {
+      if (!err) {
+        result = result.affectedRows > 0 ? "Success" : "Failure";
+        res.json({ response: result });
+      } else if (err.code === "ER_DUP_ENTRY") {
+        res.json({ response: "User already exists" });
+      } else {
+        throw err;
+      }
+    }
+  );
 });
 
 module.exports = app;
