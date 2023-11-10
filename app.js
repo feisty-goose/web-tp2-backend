e832fe7bc2e6b7502419a71e8df7de48c791005f
LarrieGans <112956859+LarrieGans@users.noreply.github.com>
2023-11-10 08:34:42 -0500

    Changed origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://web-tp2-frontend-git-main-feisty-goose.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   })
